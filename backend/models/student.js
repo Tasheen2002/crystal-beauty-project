4edf2b10e1c93073591da0eb99501c0c8bc6db9e
Tasheen2002 <darshikatasheen99@gmail.com>
2025-06-27 13:13:57 +0530

    initial model files added

---
+++ backend/models/student.js
@@ -0,0 +1,11 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const studentSchema = mongoose.Schema({
+  name: String,
+  age: Number,
+  gender: String,
+});
+
+const Student = mongoose.model("student", studentSchema);
+export default Student;
