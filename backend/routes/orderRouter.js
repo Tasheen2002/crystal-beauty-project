551589f06cf7b40c139eb5e9197c25c7d553a219
Tasheen2002 <darshikatasheen99@gmail.com>
2025-06-29 22:00:44 +0530

    create get order function

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { createOrder } from "../controller/orderController";
+import { createOrder, getOrders } from "../controller/orderController.js";
 
-const orderRouter=express.Router();
-orderRouter.post("/",createOrder);
+const orderRouter = express.Router();
+
+orderRouter.post("/", createOrder);
+orderRouter.get("/",getOrders);
 
 export default orderRouter;
