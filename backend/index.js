135a5de8a92ba5e6cc25591edbef61d6271ca6b9
Tasheen2002 <darshikatasheen99@gmail.com>
2025-06-29 21:32:44 +0530

    updated index.js according to order

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import userRouter from "./routes/userRouter.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import productRouter from "./routes/productRouter.js";
+import orderRouter from "./routes/orderRouter.js";
 
 dotenv.config();
 const app = express();
@@ -38,6 +39,7 @@ app.use((req, res, next) => {
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
