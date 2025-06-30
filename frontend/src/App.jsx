46f8dfe53bef3e67b19a570bbafee8a7366569f5
Tasheen2002 <darshikatasheen99@gmail.com>
2025-07-01 02:43:21 +0530

    login component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,19 @@ import LoginPage from "./pages/loginPage.jsx";
 import HomePage from "./pages/homePage.jsx";
 import SignUpPage from "./pages/signupPage.jsx";
 import AdminHomePage from "./pages/adminHomePage.jsx";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Toaster position="top-right"/>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/*" element={<HomePage />} />
-          <Route path="/admin/*" element={<AdminHomePage/>} />
+          <Route path="/admin/*" element={<AdminHomePage />} />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -1,85 +1,57 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("Enter your email");
+  const [password, setPassword] = useState("Enter your password");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert(`Email: ${email}\nPassword: ${password}`);
-  };
+  function login() {
+    axios
+      .post("http://localhost:3000/api/users/login", {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        if (res.data.user == null) {
+          toast.error(res.data.message);
+          return;
+        }
+        toast.success(res.data.message);
+        localStorage.setItem("token", res.data.token);
+        if (res.data.user.type == "admin") {
+          window.location.href = "/admin";
+        } else {
+          window.location.href = "/";
+        }
+      });
+  }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-sm">
-        <h1 className="text-3xl font-bold text-center text-blue-700 mb-2">
-          GameCart
-        </h1>
-        <p className="text-center text-gray-600 mb-6">
-          Welcome back! Please login to your account.
-        </p>
+    <div className="flex items-center justify-center w-full h-screen bg-red-900">
+      <div className="w-[450px] h-[450px] bg-blue-500 flex flex-col items-center justify-center">
+        <img src="/logo.png" className="rounded-full w-[100px] " />
+        <span>Email</span>
+        <input
+          defaultValue={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
 
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-gray-700 font-semibold mb-2"
-            >
-              Email:
-            </label>
-            <input
-              id="email"
-              type="email"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-            />
-          </div>
+        <span>Password</span>
+        <input
+          type="password"
+          defaultValue={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
 
-          <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-gray-700 font-semibold mb-2"
-            >
-              Password:
-            </label>
-            <input
-              id="password"
-              type="password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
-            />
-          </div>
-
-          <div className="text-right text-sm mb-4">
-            <Link
-              to="/forgot-password"
-              className="text-blue-600 hover:underline"
-            >
-              Forgot password?
-            </Link>
-          </div>
-
-          <button
-            type="submit"
-            className="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-200"
-          >
-            Log In
-          </button>
-        </form>
-
-        <p className="text-sm text-center text-gray-600 mt-4">
-          Don’t have an account?{" "}
-          <Link to="/register" className="text-blue-600 hover:underline">
-            Register
-          </Link>
-        </p>
+        <button className="bg-white" onClick={login}>
+          Login
+        </button>
       </div>
     </div>
   );
