74265cf2f07059bfc9d0ea12242f6ec872ef3bb3
Tasheen2002 <darshikatasheen99@gmail.com>
2025-06-30 22:44:38 +0530

    update according to created pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/loginPage.jsx";
 import HomePage from "./pages/homePage.jsx";
+import SignUpPage from "./pages/signupPage.jsx";
+import AdminHomePage from "./pages/adminHomePage.jsx";
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SigninPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/*" element={<HomePage />} />
+          <Route path="/admin/*" element={<AdminHomePage/>} />
         </Routes>
       </BrowserRouter>
     </>
